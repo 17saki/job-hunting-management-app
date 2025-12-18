49406877e1142543302396ec90c18fd09351afb6
17saki <159793114+17saki@users.noreply.github.com>
2025-12-18 07:13:14 +0000

    Add job-hunting-management-app

--- job-hunting-management-app/config/puma.rb
+++ job-hunting-management-app/config/puma.rb
@@ -1,3 +1,13 @@
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+threads min_threads_count, max_threads_count
+port        ENV.fetch("PORT") { 3001 }
+environment ENV.fetch("RAILS_ENV") { "development" }
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
+preload_app!
+plugin :tmp_restart
+
 # This configuration file will be evaluated by Puma. The top-level methods that
 # are invoked here are part of Puma's configuration DSL. For more information
 # about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
